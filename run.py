d305665b94a4907c92478fc1ca799ee0f02e5ef7
Felippe Roza <felippesroza@gmail.com>
2021-12-17 13:08:54 +0100

    added argparser

--- run.py
+++ run.py
@@ -2,39 +2,45 @@ from core.safety_layer import SafetyLayer
 from core.envs import Highway
 from core.callbacks import TensorboardCallback
 from stable_baselines3 import DDPG, PPO
+import argparse
 import os
 
-# config
-exp_name = 'exp_name'
-rl_algorithm = 'PPO'
-headless = True             # avoid displaying video output, suitable for running on a server
-use_safety_layer = True
-sl_weight_folder = None     # folder with safety layer weights for loading. If None it'll train a new model
-buffer_path = None          # change if there is a buffer saved
-buffer_size = 500000
-batch_size = 32
-n_epochs = 5
-training_steps = 100000           # number of training steps for the agent
+
+parser = argparse.ArgumentParser(description='Constrained Learning RL.')
+parser.add_argument('--exp_name', default='exp1', help='name of the experiment.')
+parser.add_argument('--rl_alg', default='DDPG', choices=['DDPG', 'PPO'], help='RL training algorithm.')
+parser.add_argument('--train_steps', default=100000, type=int, help='RL algorithm training steps.')
+parser.add_argument('--headless', default=True, type=bool, help='No video output, suitable for running on a server.')
+parser.add_argument('--use_safety_layer', default=True, type=bool, help='Use a safety layer or not.')
+parser.add_argument('--sl_load_folder', default=None,
+                    help='Folder with safety layer weights. If not given, a new safety layer will be trained.')
+parser.add_argument('--buffer_path', default=None, help='Path with buffer collected previously.')
+parser.add_argument('--buffer_size', default=500000, type=int, help='Buffer size (number of samples).')
+parser.add_argument('--batch_size', default=256, type=int, help='Batch size for training safety layer.')
+parser.add_argument('--n_epochs', default=20, type=int, help='Number of epochs to train safety layer.')
+args = parser.parse_args()
 
 # prepare folders and environment
-exp_dir = os.path.join('experiments', rl_algorithm + '_' + exp_name)
+exp_dir = os.path.join('experiments', args.rl_alg + '_' + args.exp_name)
 os.makedirs(exp_dir, exist_ok=True)
 
 env = Highway(mode='continuous', safety_layer=None)
-if headless:
+if args.headless:
     env.config_mode('headless')
 
-if use_safety_layer:
+if args.use_safety_layer:
     env.config_mode('discrete')
-    safe_layer = SafetyLayer(env, buffer_size, n_epochs=n_epochs, batch_size=batch_size)
-    if sl_weight_folder:
-        safe_layer.load(sl_weight_folder)
+    safe_layer = SafetyLayer(env, args.buffer_size, n_epochs=args.n_epochs, batch_size=args.batch_size)
+    if args.sl_load_folder:
+        safe_layer.load(args.sl_load_folder)
     else:
-        if buffer_path is None:
+        if args.buffer_path is None:
             buffer_path = os.path.join(exp_dir, 'buffer.obj')
             safe_layer.collect_samples()  # not necessary if there is already a buffer saved
             safe_layer.save_buffer(buffer_path)  # save buffer with collected samples
-        safe_layer.load_buffer(buffer_path)
+            safe_layer.load_buffer(buffer_path)
+        else:
+            safe_layer.load_buffer(args.buffer_path)
         safe_layer.train()
         safe_layer.save(os.path.join(exp_dir, 'SafetyLayer'))
 
@@ -45,11 +51,11 @@ else:
     safety_status = 'unsafe'
 
 # train RL agent
-if rl_algorithm == 'PPO':
-    rl_agent = PPO("MlpPolicy", env, verbose=1, tensorboard_log=os.path.join('experiments','tensorboard'))
-elif rl_algorithm == 'DDPG':
+if args.rl_alg == 'PPO':
+    rl_agent = PPO("MlpPolicy", env, verbose=1, tensorboard_log=os.path.join('experiments', 'tensorboard'))
+elif args.rl_alg == 'DDPG':
     rl_agent = DDPG("MlpPolicy", env, verbose=1, tensorboard_log=os.path.join('experiments', 'tensorboard'))
 
-rl_agent.learn(total_timesteps=training_steps, tb_log_name=safety_status + rl_algorithm + exp_name,
+rl_agent.learn(total_timesteps=args.train_steps, tb_log_name=safety_status + args.rl_alg + args.exp_name,
                callback=TensorboardCallback(env))
 rl_agent.save(os.path.join(exp_dir, safety_status))
